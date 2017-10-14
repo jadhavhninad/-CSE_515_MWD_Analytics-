cc6b60d5248135bc4f19aa47aa04e3c31e3f139a
jadhavhninad <jadhavhninad@gmail.com>
2017-10-13 20:43:43 -0700

    Phase2 - movie recommender

--- Phase 1/Project Code/phase1_code/mysqlConn.py
+++ Phase 1/Project Code/phase1_code/mysqlConn.py
@@ -5,8 +5,8 @@ import MySQLdb
 
 class DbConnect():
     def __init__(self):
-        #self.conn = MySQLdb.connect(host='localhost', user='root', passwd='abc123', db='mwd_phase1')
-        self.conn = MySQLdb.connect(host='localhost', user='root', passwd='abc123', db='mwd_p1test')
+        self.conn = MySQLdb.connect(host='localhost', user='root', passwd='abc123', db='mwd_phase1')
+        #self.conn = MySQLdb.connect(host='localhost', user='root', passwd='abc123', db='mwd_p1test')
         self.cur = self.conn.cursor()
 
     def get_connection(self):
---
+++ Phase 2/phase2Code/mysqlConn.py
@@ -0,0 +1,33 @@
+import MySQLdb
+
+# open a database connection
+# be sure to change the host IP address, username, password and database name to match your own
+
+class DbConnect():
+    def __init__(self):
+        self.conn = MySQLdb.connect(host='localhost', user='root', passwd='abc123', db='mwd_phase2')
+        #self.conn = MySQLdb.connect(host='localhost', user='root', passwd='abc123', db='mwd_p1test')
+        self.cur = self.conn.cursor()
+
+    def get_connection(self):
+        return self.conn
+
+    def db_query(self):
+        query = "show tables"
+        self.cur.execute(query)
+        self.result = self.cur.fetchone()
+        return self.result
+
+    def db_exit(self):
+        self.conn.close()
+
+if __name__ == "__main__":
+    db_conn = DbConnect()
+    value = db_conn.db_query()
+    print(value)
+    db_conn.db_exit()
+
+
+
+
+
